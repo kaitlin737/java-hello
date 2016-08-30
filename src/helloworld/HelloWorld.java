6d687f0a10d3388bdf46b912cced258a53605db4
kcstark <kcstark@WS205-008.ad.coloradomesa.edu>
2016-08-30 12:09:22 -0600

    basic hello version

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+import static kiss.API.*;
+
+/**
+ *
+ * @author kcstark
+ */
+public class HelloWorld {
+    
+    void untestRun() {
+        outExpect("Hello!");
+        run();
+        outClose();
+    }
+    
+
+  void run() {
+      println("Hello!");
+  }
+   
+}
