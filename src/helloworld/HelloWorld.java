ecde822e4e9c9bbce550e947885a51158bdecc0c
kcstark <kcstark@WS205-008.ad.coloradomesa.edu>
2016-09-15 11:10:43 -0600

    test strings

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -137,7 +137,90 @@ void testInts(){
             
 }
 
-void testloop(){
+void testLoop(){
+  //  try(Close out =outExpect(0,EOL,1,EOL,3,EOL)){
+    //int n=4;
+    //int i=0;
+    //while (i<n){
+      //  if(i==2){ ++i; break;}
+        //println(i);
+        //++i;//use ++i instead of i++
+        
+  //  }
+//}
+    //try(Close out =outExpect(0,EOL,1, EOL,2, EOL,3,EOL)){
+    //int n=4;
+    //for (int i=0; i<n; ++i) {
+        //if(i==2) continue;
+        //println(i);
+
+    String [] words= new String[] {"this", "that", "other"};
+    assert words[0].equals("this");
+    assert words[1].equals("that");
+    assert words[2].equals("other");
+    assert words.length == 3;
     
+    String [] nouns = new String[2];
+    assert nouns[0]== null;
+    assert nouns[1]== null;
+    assert nouns.length== 2;
+    
+    try(Close out=outExpect("this",EOL,"that",EOL,"other",EOL)){
+    for(int i=0; i<words.length;++i){
+        println(words[i]);
+    }
+    }
+    
+    try(Close out=outExpect("this",EOL,"that",EOL,"other",EOL)){
+    for (String word : words){
+        println(word);
+    }
+}
+}
+int  instanceValue = 0;
+
+void methodExample(){
+    int localValue =0;
+    ++instanceValue;
+    ++localValue;
+    println("I", instanceValue, "L", localValue);
+}
+
+long factorial(int n){
+    if (n>1) {
+        return n*factorial(n-1);
+    }else{
+        return 1;
+    }
+}
+void testFunctions(){
+    
+    try (Close out= outExpect(
+            "I", 1, "L", 1, EOL,
+            "I", 2, "L", 1, EOL,  
+            "I", 3, "L", 1, EOL)){
+    methodExample();
+    methodExample(); 
+    methodExample();
+}
+    assert factorial(1) == 1;
+    assert factorial(5) == 120;
+}
+
+void testString(){
+    String hi = "hello";
+    assert hi.length() == 5;
+    assert hi.substring(1,3).equals("el");//[a,b),== for primative type
+    
+   hi = hi + "world";
+   
+   StringBuilder sb = new StringBuilder();
+   sb.append("hello");
+   sb.append(" world");
+   sb.append( " #");
+   sb.append(13);
+   String hw = sb.toString();
+   println(hw);
+   
 }
 }
