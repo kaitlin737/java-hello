278d54a45023f91d130262d5e1d583bcb3093c1e
kcstark <kcstark@WS205-008.ad.coloradomesa.edu>
2016-10-13 08:56:21 -0600

    collections arraylist

---
+++ src/Javacollections.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author kcstark
+ */
+public class Javacollections {
+    public int compare (Integer a,Integer b){
+        return -a.compareTo(b);
+    }
+  
+    public static void main(String[] args) {
+        ArrayList<String> name = new ArrayList();
+        name.add("Max");
+        name.add("John");
+        name.add("Jake");
+        assert(name.size()==3);
+        assert(name.isEmpty()==false);
+        name.add("Jacob");
+        assert(name.size()==4);
+        System.out.println(name);
+    }
+}
