418495562e5fcd463b23f0f696e99fadf3d01c6e
kcstark <kcstark@WS205-008.ad.coloradomesa.edu>
2016-11-04 15:04:33 -0600

    junit tests

--- src/helloworld/Tests.java
+++ src/helloworld/Tests.java
@@ -1,17 +1,7 @@
 package helloworld;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author kcstark
- */
 public class Tests {
- import org.junit.Test;
+import org.junit.Test;
 import static org.junit.Assert.*;
  
 public class MyAssertEqualsTest {
@@ -30,6 +20,7 @@ public class MyAssertEqualsTest {
       int addition = grade1 + grade2;
       Assert.assertTrue(addition == 157);
    }
+    
     @Test
     public void testDivision(){
       double grade1=60;
