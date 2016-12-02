635a6a397832307db69da6268d61232b1fe5b98b
kcstark <kcstark@WS205-008.ad.coloradomesa.edu>
2016-12-01 17:32:06 -0700

    threading

--- src/assignment5/App.java
+++ src/assignment5/App.java
@@ -4,14 +4,11 @@
  * and open the template in the editor.
  */
 package assignment5;
-import assignment5.ConsoleClock;
-import assignment5.Employee;
-import assignment5.Manager;
-import assignment5.duty;
 import static kiss.API.*;
 
 
 public class App {
+    
          void testEmployees(){
            duty duty = new duty();
            duty.addduty("clean");
@@ -36,27 +33,27 @@ public class App {
             Larry.beginshift();
             Larry.endShift();
         }
-        class Worker implements Runnable{
-            @Override
+         public class Countdown extends Thread{
+            
+             public void run(){
+                 println("begin");
+             }   
+         }
+        public class countdown implements Runnable{
             public void run(){
-                println("Started");
-                pause(3);
-                println("Done");
+                println("Started Shift");
+                pause(10);
+                println("Done With Shift");
             }
         }
-    void run(){
-        new Thread(()-> {pause(2);println("Begin shift"); }).start();
-        Worker worker=new Worker();
-        Thread workerThread = new Thread(worker);
-        workerThread.start();
-        new Thread(new Worker()).start();
-        ConsoleClock clock = new ConsoleClock();
-        clock.start();
-        pause(4.0);
-        clock.close();
-        println("done");
+        public static void main() {
+                 Thread thread =new Thread();
+                 thread.start();
+                  
+            }
+ 
     }
-}
+
 
 
 
